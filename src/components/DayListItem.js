3f1e667e2d252a3ddfb1c86427e004c5f308cf39
Justin Stewart <justinstewart@Justins-MacBook-Air.local>
2019-11-05 16:35:10 -0800

    cypress testing now complete

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,7 +8,7 @@ export default function DayListItem(props) {
     'day-list__item--full': !props.spots
   })
   
-  return (<li className={dayClass} onClick={() => props.setDay(props.name)}><h2 className="text--regular">{props.name}</h2><h3 className="text--light">{formatSpots(props.spots)}</h3></li>);
+  return (<li className={dayClass} data-testid="day" onClick={() => props.setDay(props.name)}><h2 className="text--regular">{props.name}</h2><h3 className="text--light">{formatSpots(props.spots)}</h3></li>);
 }
 
 const formatSpots = function(spots) {
